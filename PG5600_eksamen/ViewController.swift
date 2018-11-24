7337e4b7436019810a7cab075eedcfcf19236b7b
olemartinlarsen <olemartinlarsen@outlook.com>
2018-11-24 16:52:17 +0100

    Initial Commit

---
+++ PG5600_eksamen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PG5600_eksamen
+//
+//  Created by Ole Martin Larsen on 24/11/2018.
+//  Copyright © 2018 Høyskolen Kristiania. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
